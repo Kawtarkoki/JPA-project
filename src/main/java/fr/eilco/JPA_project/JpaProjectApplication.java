0dee8acc8a42807652e380ea6fea8b8e8e265206
Kawtarkoki <akawtarbakour@gmail.com>
2022-02-01 00:49:51 +0100

    version 2

--- src/main/java/fr/eilco/JPA_project/JpaProjectApplication.java
+++ src/main/java/fr/eilco/JPA_project/JpaProjectApplication.java
@@ -28,8 +28,8 @@ public class JpaProjectApplication implements CommandLineRunner{
 		
 		// Insertion des patients dans ma base de donnée 
 		patientRepository.save(new Patient(null,"kawtar",new Date() ,2300,false)); // il faut respecter l'ordre des attributs de la classe Patient
-		patientRepository.save(new Patient(null,"houda",new Date() ,2300,false));	
-		patientRepository.save(new Patient(null,"nada",new Date() ,2300,true));
+		//patientRepository.save(new Patient(null,"houda",new Date() ,2300,false));	
+		//patientRepository.save(new Patient(null,"nada",new Date() ,2300,true));
 		//afficher pour chaque patient p ja vais afficher ses infos 
 		patientRepository.findAll().forEach(p->{
 			System.out.println(p.toString());
